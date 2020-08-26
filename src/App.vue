3d7083636abc0c45f5ce9c73d07d8438f37dcda8
Nitika Mishra <nitika.mishra@coditas.com>
2020-08-26 20:20:28 +0530

    added carousel

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/" exact>Home</router-link>
-    </div>
-    <router-view/>
+    <the-carousel></the-carousel>
   </div>
 </template>
 
-<script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+<script>
+import TheCarousel from './components/TheCarousel.vue';
 
-@Component({})
-export default class App extends Vue {
-
-}
+export default {
+  name: 'App',
+  components: {
+    TheCarousel,
+  },
+};
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheCarousel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="carousel">
+    <carousel :per-page="1" :mouse-drag="true">
+      <slide v-for="item in carouselItem" :key="item.id" :tabindex="item.id">
+        <div class="container">
+          <img
+            class="trekking"
+            v-bind:src="require('../assets/' + item.image)"
+            v-bind:alt="item.image"
+          />
+          <div class="text">
+            <div class="sub-text">{{ item.textOverImage }}</div>
+          </div>
+        </div>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+  name: 'TheCarousel',
+  components: {
+    Carousel,
+    Slide,
+  },
+  data: function() {
+    return {
+      carouselItem: [
+        {
+          image: 'trekking.png',
+          textOverImage: 'Slide 1',
+          id: 1,
+        },
+        {
+          image: 'trekking.png',
+          textOverImage: 'Slide 2',
+          id: 2,
+        },
+      ],
+    };
+  },
+  methods: {
+    getImgUrl: function(path) {
+      console.log(path);
+      return require(path);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.container {
+  position: relative;
+}
+
+.trekking {
+  object-fit: cover;
+  width: 100%;
+  height: 500px;
+}
+
+.text {
+  position: absolute;
+  color: white;
+  top: 25%;
+  left: 25%;
+}
+</style>
