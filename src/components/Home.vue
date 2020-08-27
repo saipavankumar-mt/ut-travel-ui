814e9fbcf5f9f5d7db2c7eb002ced75a3e6829ea
Nitika Mishra <nitika.mishra@coditas.com>
2020-08-27 19:05:48 +0530

    added home component

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <the-carousel></the-carousel>
+    <offer-list></offer-list>
+  </div>
+</template>
+
+<script>
+import TheCarousel from "./TheCarousel.vue";
+import OfferList from "./OfferList.vue";
+export default {
+  name: "Home",
+  components: {
+    TheCarousel,
+    OfferList,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
