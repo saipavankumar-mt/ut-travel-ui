04a9af80055b10fe698e11ea27448e61e24bd8e2
Nitika Mishra <nitika.mishra@coditas.com>
2020-08-29 13:23:02 +0530

    added travel detail page

---
+++ src/components/ui-components/TravelDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="travel-detail">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TravelDetail',
+  components: {},
+  data() {
+    return {
+      detailData: {},
+    };
+  },
+  created: function() {
+    const baseUrl = process.env.BASE_URL;
+    this.$http.get(`${baseUrl}Data/Pckg4-0.json`).then((res) => {
+      this.detailData = res;
+    });
+    console.log(this.detailData);
+  },
+};
+</script>
+
+<style lang="scss"></style>
