0a56c956aa3f62320177a5d575c803ad475e0da0
Nitika Mishra <nitika.mishra@coditas.com>
2020-08-30 13:06:43 +0530

    css changes

--- src/components/ui-components/TravelDetail.vue
+++ src/components/ui-components/TravelDetail.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="travel-detail">
-    <div class="columns">
-      <div class="column is-one-quarter">
-        <img
-          class="image"
-          src="../../assets/images/destinations/nainital.webp"
-          alt="destination"
-        />
+    <div class="travel-detail-container">
+      <div class="image-container">
+        <img class="image" src="../../assets/images/destinations/nainital.webp" alt="destination" />
       </div>
       <div class="detail-container">
         <div class="detail-title">
-          <div class="title is-3">
-            {{ posts.title }}
-          </div>
-          <p class="quote subtitle is-5 is-italic">
-            {{ posts.qoute }}
-          </p>
+          <div class="title is-3">{{ posts.title }}</div>
+          <p class="quote subtitle is-5 is-italic">{{ posts.qoute }}</p>
         </div>
         <div class="description subtitle is-5">{{ posts.subtitle }}</div>
         <app-divider></app-divider>
         <div class="itinerary">
-          <div><span id="name">Route: </span> {{ posts.route }}</div>
-          <div><span id="name">Duration: </span> {{ posts.duration }}</div>
-          <div><span id="name">Night Stay:</span> {{ posts.nightStay }}</div>
-          <div
-            class="days-container"
-            v-for="item in posts['Description']"
-            :key="item.id"
-          >
-            <div class="days">{{ item.title }}</div>
-            <div>
-              <ui
-                class="day-route"
-                v-for="(title, index) in item.subtitles"
-                :key="index"
-              >
-                <li>{{ title }}</li>
-              </ui>
-            </div>
+          <div>
+            <span id="name">Route:</span>
+            {{ posts.route }}
+          </div>
+          <div>
+            <span id="name">Duration:</span>
+            {{ posts.duration }}
+          </div>
+          <div>
+            <span id="name">Night Stay:</span>
+            {{ posts.nightStay }}
           </div>
         </div>
+      </div>
+    </div>
+    <div>
+      <div class="days-container" v-for="item in posts['Description']" :key="item.id">
+        <div class="days">{{ item.title }}</div>
+        <ul>
+          <li class="day-route" v-for="(title, index) in item.subtitles" :key="index">{{ title }}</li>
+        </ul>
       </div>
     </div>
 
@@ -48,11 +41,11 @@
 </template>
 
 <script>
-import axios from 'axios';
-import AppDivider from './AppDivider.vue';
+import axios from "axios";
+import AppDivider from "./AppDivider.vue";
 
 export default {
-  name: 'TravelDetail',
+  name: "TravelDetail",
   components: {
     AppDivider,
   },
@@ -82,40 +75,53 @@ export default {
 <style lang="scss">
 .travel-detail {
   position: relative;
-  .columns {
-    margin: 0 !important;
-  }
+  padding: 40px;
+  .travel-detail-container {
+    display: flex;
+    .image-container {
+      width: 100%;
+      // padding: 52px 0 52px 52px;
+    }
 
-  .detail-container {
-    padding: 32px;
-    .quote {
-      margin-left: 45%;
-      color: red;
-    }
-    .detail-title {
-      padding-bottom: 26px;
-    }
-    .itinerary {
-      padding: 12px;
-      color: white;
-      font-weight: 700;
-      .day-route {
+    .detail-container {
+      //padding: 32px;
+      .quote {
+        margin-left: 45%;
+        color: red;
       }
-      .days-container {
+      .detail-title {
+        padding-bottom: 26px;
+      }
+      .itinerary {
+        padding: 12px;
+        // color: white;
+        font-weight: 700;
         text-align: left;
-        padding: 24px 16px;
-        .days {
-          color: darkred;
+        .day-route {
         }
       }
+      .description {
+        color: #382d29;
+      }
     }
-    .description {
-      color: #382d29;
+  }
+
+  .days-container {
+    text-align: left;
+    padding: 24px 16px;
+    .days {
+      color: darkred;
+      font-weight: 700;
     }
   }
 
   #name {
     color: darkred;
+  }
+
+  ul {
+    list-style: disc;
+    margin-left: 40px;
   }
 }
 </style>
