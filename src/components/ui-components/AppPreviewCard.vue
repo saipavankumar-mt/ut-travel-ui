11b6ce100b64aff091dc03ac2c1c462034e23f5d
kalyanpraveen <kalyankerpraveen@gmail.com>
2020-08-31 02:34:57 +0530

    Fix issue with dynamic image url loading & add hover effect

--- src/components/ui-components/AppPreviewCard.vue
+++ src/components/ui-components/AppPreviewCard.vue
@@ -2,8 +2,7 @@
   <div class="preview-card card" v-on:click="item.id ?redirect():''">
     <div class="card-image">
       <figure class="image is-4by3">
-        <!-- <img :src="'/img/'+item.image"> -->
-        <img src="../../assets/images/destinations/nainital.webp" />
+        <img :src="getImageUrl" :alt="item.image" />
       </figure>
     </div>
     <div class="card-content">
@@ -17,18 +16,29 @@
 
 <script>
 export default {
-  name: "AppPreviewCard",
+  name: 'AppPreviewCard',
   props: {
     item: { type: Object, required: true },
-    image: { type: String },
+    type: { type: String },
   },
-  created() {
-    console.log({ item: this.item });
+  computed: {
+    getImageUrl() {
+      if (!this.item.image) {
+        return;
+      }
+      // TODO: create folders for each type eg: destinations/[name].png , packages/[name].png
+      // const type = this.item.type;
+      // const fileName = this.item.image.name.toLowerCase();
+      // const ext = this.item.image.ext;
+
+      // return require(`../../assets/images/${type}/${fileName}.${ext}`);
+      return require('../../assets/images/destinations/nainital.webp');
+    },
   },
   methods: {
     redirect: function () {
       this.$router.push({
-        name: "Detail",
+        name: 'Detail',
         params: { packageId: this.item.id },
       });
     },
@@ -38,12 +48,24 @@ export default {
 
 <style lang="scss">
 .preview-card {
+  min-height: 20rem;
+  height: 100%;
   border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+
   .image img {
     border-radius: 5px 5px 0 0;
   }
   &:hover {
     cursor: pointer;
+    box-shadow: 0 0 15px 2px;
+    z-index: 50;
+    transform: scale(1.08);
+    .image img { 
+      transform: scale(1.05);
+      border-radius: 0;
+      transition: all 0.1s ease-in-out;
+    }
   }
 }
 </style>
