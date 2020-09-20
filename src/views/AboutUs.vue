6e099c11756af1fbf3c8f43e3f3fdbe79a29accd
mis-nitika <nitika280993@gmail.com>
2020-09-20 22:28:25 +0530

    css fixes

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,103 +1,96 @@
 <template>
   <div class="about-us">
-    <div class="hero-body">
-      <div>
-        <div class="heading">
-          <h1><span>About</span> Us</h1>
+    <div>
+      <div class="heading">
+        <h1><span>About</span> Us</h1>
+      </div>
+      <h5>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        efficitur sit amet massa fringilla egestas. Nullam condimentum luctus
+        turpis.
+      </h5>
+      <article class="media profile" v-for="(item, idx) in founder" :key="idx">
+        <div class="media-left">
+          <figure class="image is-128x128 ">
+            <img
+              class="is-rounded"
+              v-bind:src="require('../assets/images/' + item.image)"
+              alt="Image"
+            />
+          </figure>
         </div>
-        <h5>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          efficitur sit amet massa fringilla egestas. Nullam condimentum luctus
-          turpis.
-        </h5>
-        <article
-          class="media profile"
-          v-for="(item, idx) in founder"
-          :key="idx"
-        >
-          <div class="media-left">
-            <figure class="image is-128x128 ">
-              <img
-                class="is-rounded"
-                v-bind:src="require('../assets/images/' + item.image)"
-                alt="Image"
-              />
-            </figure>
+        <div class="media-content">
+          <div class="content">
+            <p style="text-align:left">
+              <strong>{{ item.name }}</strong
+              ><br />
+              <small>{{ item.designation }}</small
+              ><br />
+              <small>{{ item.location }}</small
+              ><br />
+              <small>
+                <br />M. Rajkumar is a travel ethusiast who has an absolute
+                passion for exploring places around him. He is the one to
+                consult if you are stuck at place X in Uttaranchal and wanna
+                reach place Y via place z. If ever an 'Uttaranchal Awareness
+                quiz is held on the planet, he will be the undisputed winner of
+                the competition for he has immense knowledge about the state. He
+                holds an MBA degree from Gurukul Kangri University and worked
+                with Uttaranchal Government for sometime. However, the desire to
+                start a venture of his own sent him to pack bags and jump into
+                the battlefield of travel agents. <br />M. Rajkumar has an
+                extensive knowledge about Uttaranchal. He has personally visited
+                almost all the properties and places where Uttaranchal Holidays
+                accommodates its clients. This fact, together with his hobby of
+                exploring places makes him more capable of handling issues
+                guests are likely to face while travelling. So, Uttaranchal
+                Holidays isn't money-creating machine for its operators but a
+                vehicle to help people know Uttaranchal more This competitive
+                advantage gets reflected in the services Uttaranchal Holidays
+                provides I hope I have told you enough about Uttaranchal
+                Holidays Now is the time for you to give us a chance to serve
+                you. So, come and experience the difference we claim to offer
+                shd get a trip booked with Uttaranchal Holidays.NOW
+              </small>
+            </p>
           </div>
-          <div class="media-content">
-            <div class="content">
-              <p style="text-align:left">
-                <strong>{{ item.name }}</strong
-                ><br />
-                <small>{{ item.designation }}</small
-                ><br />
-                <small>{{ item.location }}</small
-                ><br />
-                <small>
-                  <br />M. Rajkumar is a travel ethusiast who has an absolute
-                  passion for exploring places around him. He is the one to
-                  consult if you are stuck at place X in Uttaranchal and wanna
-                  reach place Y via place z. If ever an 'Uttaranchal Awareness
-                  quiz is held on the planet, he will be the undisputed winner
-                  of the competition for he has immense knowledge about the
-                  state. He holds an MBA degree from Gurukul Kangri University
-                  and worked with Uttaranchal Government for sometime. However,
-                  the desire to start a venture of his own sent him to pack bags
-                  and jump into the battlefield of travel agents. <br />M.
-                  Rajkumar has an extensive knowledge about Uttaranchal. He has
-                  personally visited almost all the properties and places where
-                  Uttaranchal Holidays accommodates its clients. This fact,
-                  together with his hobby of exploring places makes him more
-                  capable of handling issues guests are likely to face while
-                  travelling. So, Uttaranchal Holidays isn't money-creating
-                  machine for its operators but a vehicle to help people know
-                  Uttaranchal more This competitive advantage gets reflected in
-                  the services Uttaranchal Holidays provides I hope I have told
-                  you enough about Uttaranchal Holidays Now is the time for you
-                  to give us a chance to serve you. So, come and experience the
-                  difference we claim to offer shd get a trip booked with
-                  Uttaranchal Holidays.NOW
-                </small>
-              </p>
-            </div>
+        </div>
+      </article>
+      <section>
+        <div>
+          <div class="heading">
+            <h1><span>Our</span> Team</h1>
           </div>
-        </article>
-        <section>
           <div>
-            <div class="heading">
-              <h1><span>Our</span> Team</h1>
-            </div>
-            <div>
-              <header class="team-heading">
-                <h1 class="title">Marketing Team</h1>
-              </header>
-            </div>
-            <div class="columns is-multiline" size="is-medium">
-              <div
-                class="column is-one-third"
-                v-for="(item, idx) in marketing"
-                :key="idx"
-              >
-                <app-team :member="item"></app-team>
-              </div>
-            </div>
+            <header class="team-heading">
+              <h1 class="title">Marketing Team</h1>
+            </header>
           </div>
-        </section>
-        <section>
-          <header class="team-heading">
-            <h1 class="title">Operation Team</h1>
-          </header>
           <div class="columns is-multiline" size="is-medium">
             <div
               class="column is-one-third"
-              v-for="(item, idx) in operations"
+              v-for="(item, idx) in marketing"
               :key="idx"
             >
               <app-team :member="item"></app-team>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+      <section>
+        <header class="team-heading">
+          <h1 class="title">Operation Team</h1>
+        </header>
+        <div class="columns is-multiline" size="is-medium">
+          <div
+            class="column is-one-third"
+            v-for="(item, idx) in operations"
+            :key="idx"
+          >
+            <app-team :member="item"></app-team>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -137,6 +130,7 @@ export default {
 
 <style lang="scss">
 .about-us {
+  padding: 0 2rem;
   .image.is-128x128 {
     height: 168px;
     width: 168px;
@@ -160,9 +154,10 @@ export default {
   h5 {
     font: 16px;
     color: #394048;
-    padding: 0 20px 20px 20px;
+    padding: 0 180px 20px 180px;
     font-weight: 700;
     line-height: 1.2;
+    color: #60bff3;
   }
 
   .team-heading {
