fae44660196724b2000fcca2de6494c25c73bb2c
kalyanpraveen <kalyankerpraveen@gmail.com>
2020-09-24 00:05:43 +0530

    About us page changes

---
+++ src/components/ui-components/AppProfileCard.vue
@@ -0,0 +1,123 @@
+<template>
+  <article
+    class="profile-card"
+    :class="{ 'founder': isFounder, 'marketing': isMarketing, 'operations': isOperations }"
+  >
+    <figure class="media-left image member-image">
+      <img
+        class="is-rounded"
+        :src="require('../../assets/images/aboutus/' + member.image)"
+        :alt="member.image"
+      />
+    </figure>
+    <aside class="content has-text-left member-info">
+      <div class="member-name">{{ member.name }}</div>
+      <div class="member-designation">{{ member.designation }}</div>
+      <div class="member-location">{{ member.location }}</div>
+      <div class="member-desc">
+        <p>{{member.description}}</p>
+        <p v-if="member.description_extra">{{member.description_extra}}</p>
+      </div>
+    </aside>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'AppProfileCard',
+  props: {
+    member: { type: Object, required: true },
+    isFounder: { type: Boolean },
+    isMarketing: { type: Boolean },
+    isOperations: { type: Boolean },
+  },
+};
+</script>
+
+<style lang="scss">
+.profile-card {
+  display: flex;
+  padding: 1.25rem;
+  position: relative;
+  border-radius: 4px;
+  box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
+    0 0px 0 1px rgba(10, 10, 10, 0.02);
+  .member-image {
+    height: 192px;
+    width: 192px;
+    padding: 0.5rem;
+    background: #dbdbdb;
+    border-radius: 290486px;
+  }
+
+  .member-info {
+    border-left: 2px solid rgba(71, 202, 240, 0.25);
+    padding-left: 2rem;
+    margin-left: 1rem;
+    .member-name {
+      display: inline-block;
+      color: #60bff3;
+      font-weight: 600;
+      font-size: 1.25rem;
+      line-height: 24px;
+      margin-bottom: 0.25rem;
+    }
+
+    .member-designation {
+      display: inline-block;
+      color: #918f9b;
+      margin-bottom: 0.25rem;
+      margin-left: 0.5rem;
+      padding-left: 0.5rem;
+      border-left: 2px solid rgba(71, 202, 240, 0.5);
+    }
+
+    .member-location {
+      color: #918f9b;
+      font-style: italic;
+      font-size: 15px;
+      margin-bottom: 1.5rem;
+    }
+
+    .member-desc {
+      color: #918f9b;
+      font-size: 15px;
+      line-height: 21px;
+    }
+  }
+  &.founder {
+    margin: 0 1.75rem;
+  }
+  &.marketing {
+    padding: 1rem;
+    .member-image {
+      width: 160px;
+      height: 160px;
+    }
+    .member-info {
+      padding-left: 1.75rem;
+      margin-left: 0.75rem;
+      .member-name {
+        font-size: 1.125rem;
+      }
+    }
+  }
+  &.operations {
+    padding: 0.75rem;
+    .member-image {
+      width: 128px;
+      height: 128px;
+    }
+    .member-info {
+      padding-left: 1rem;
+      margin-left: 0.25rem;
+      .member-name {
+        font-size: 1rem;
+      }
+      .member-designation {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -8,88 +8,32 @@
       </p>
     </header>
     <section class="about-us-content">
-      <article class="media about-founder" v-for="(item, idx) in founder" :key="idx">
-        <div class="media-left has-text-left member-info ">
-          <figure class="image member-image">
-            <img
-              :src="require('../assets/images/aboutus/' + item.image)"
-              :alt="item.image"
-            />
-          </figure>
-          <div class="member-name">{{ item.name }}</div>
-          <div class="member-designation">{{ item.designation }}</div>
-          <div class="member-location">{{ item.location }}</div>
-        </div>
-        <div class="media-content content has-text-left member-desc ">
-          <p>
-            M. Rajkumar is a travel ethusiast who has an absolute
-            passion for exploring places around him. He is the one to
-            consult if you are stuck at place X in Uttaranchal and wanna
-            reach place Y via place z. If ever an 'Uttaranchal Awareness
-            quiz is held on the planet, he will be the undisputed winner of
-            the competition for he has immense knowledge about the state. He
-            holds an MBA degree from Gurukul Kangri University and worked
-            with Uttaranchal Government for sometime. However, the desire to
-            start a venture of his own sent him to pack bags and jump into
-            the battlefield of travel agents.
-          </p>
-          <p>
-            M. Rajkumar has an
-            extensive knowledge about Uttaranchal. He has personally visited
-            almost all the properties and places where Uttaranchal Holidays
-            accommodates its clients. This fact, together with his hobby of
-            exploring places makes him more capable of handling issues
-            guests are likely to face while travelling. So, Uttaranchal
-            Holidays isn't money-creating machine for its operators but a
-            vehicle to help people know Uttaranchal more This competitive
-            advantage gets reflected in the services Uttaranchal Holidays
-            provides I hope I have told you enough about Uttaranchal
-            Holidays Now is the time for you to give us a chance to serve
-            you. So, come and experience the difference we claim to offer
-            shd get a trip booked with Uttaranchal Holidays NOW
-          </p>
-        </div>
-      </article>
+      <app-profile-card
+        v-for="(member, idx) in founder"
+        :key="idx"
+        :member="member"
+        :is-founder="true"
+      ></app-profile-card>
     </section>
-    <header class="about-us-header">
+    <header class="team-header">
       <h1 class="main-title">Our Team</h1>
     </header>
-    <div>
-      <section>
-        <div>
-          <div>
-            <header class="team-heading">
-              <h1 class="title">Marketing Team</h1>
-            </header>
-          </div>
-          <div class="columns is-multiline" size="is-medium">
-            <div class="column is-one-third" v-for="(item, idx) in marketing" :key="idx">
-              <app-team :member="item"></app-team>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <header class="team-heading">
-          <h1 class="title">Operation Team</h1>
-        </header>
-        <div class="columns is-multiline" size="is-medium">
-          <div class="column is-one-third" v-for="(item, idx) in operations" :key="idx">
-            <app-team :member="item"></app-team>
-          </div>
-        </div>
-      </section>
-    </div>
+    <section class="marketing-team columns is-multiline">
+      <div class="column is-half" v-for="(member, idx) in marketing" :key="idx">
+        <app-profile-card :member="member" :is-marketing="true"></app-profile-card>
+      </div>
+    </section>
+    <section class="operations-team columns is-multiline">
+      <div class="column is-one-third" v-for="(member, idx) in operations" :key="idx">
+        <app-profile-card :member="member" :is-operations="true"></app-profile-card>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import AppTeam from '../components/ui-components/AppTeam.vue';
 export default {
   name: 'AboutUs',
-  components: {
-    AppTeam,
-  },
   data() {
     return {
       marketing: [],
@@ -118,9 +62,11 @@ export default {
 <style lang="scss">
 .about-us {
   padding: 0 2.5rem;
+  margin-bottom: 1rem;
   font: 16px/18px 'Lato', sans-serif;
 
-  .about-us-header {
+  .about-us-header,
+  .team-header {
     font-family: 'Lato', sans-serif;
     padding-top: 3rem;
     padding-bottom: 1.5rem;
@@ -155,201 +101,52 @@ export default {
     }
   }
 
-  .media {
-    padding: 1.25rem;
-    margin: 0 1.5rem;
+  .operations-team {
+    margin-top: 2rem !important;
+    padding-top: 2rem;
     position: relative;
-    border-radius: 4px;
-    box-shadow: 0.5em 0.5em 1em 0.125em rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.02);
-
-    &.about-founder {
-      .member-info {
-        position: relative;
-
-        &:after {
-          content: ' ';
-          display: block;
-          position: absolute;
-          top: 0;
-          right: -2rem;
-          width: 2px;
-          height: 100%;
-          background: rgba(145, 143, 155, 0.25);
-        }
-
-        .member-image {
-          height: 192px;
-          width: 192px;
-          margin-bottom: 1rem;
-          >img {
-            border-radius: 4px;
-          }
-        }
-
-        .member-name {
-          color: #3e3f54;
-          font-weight: 600;
-          font-size: 1.25rem;
-          line-height: 24px;
-          margin-bottom: .25rem;
-        }
-
-        .member-designation {
-          color: #918f9b;
-          margin-bottom: .25rem;
-        }
-
-        .member-location {
-          font-style: italic;
-          font-size: 15px;
-          color: #918f9b;
-        }
-      }
-
-      .member-desc {
-        color: #918f9b;
-        font-size: 15px;
-        line-height: 21px;
-        padding-left: 2rem;
-      }
+    &:before {
+      content: ' ';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      background: #47caf0;
+      height: 2px;
+      width: 6.5rem;
     }
   }
 
-  strong {
-    color: #60bff3;
-  }
-  .columns {
-    padding: unset !important;
-    .column {
-      padding-bottom: 60px;
-      .is-rounded {
-        filter: brightness(100%);
-      }
-    }
-  }
+  // @media only screen and (min-width: 360px) and (max-width: 640px) {
+  //   .about-us {
+  //     h5 {
+  //       padding: unset;
+  //       padding-bottom: 30px;
+  //     }
+  //     .media {
+  //       display: flex;
+  //       flex-direction: column;
+  //       margin: auto;
 
-  .image.is-128x128 {
-    height: 168px;
-    width: 168px;
-  }
-  .image-contianer {
-    position: absolute;
-    right: -57px;
-    bottom: 48px;
-    top: -53px;
-  }
-  h5 {
-    font: 16px;
-    padding: 0 180px 20px 180px;
-    // font-weight: 700;
-    line-height: 1.2;
-    color: #60bff3;
-  }
+  //       .media-left {
+  //         margin: auto;
+  //       }
+  //     }
+  //   }
 
-  .team-heading {
-    background: none !important;
-    padding: 4rem;
+  //   .media-content {
+  //     .content {
+  //       text-align: center;
+  //       background: white;
+  //     }
+  //   }
 
-    .title {
-      font: 1.5rem/18px 'Lato', sans-serif;
-      // font-weight: 600;
-      color: #3e3f54;
-      text-align: left;
-    }
-  }
-
-  .heading {
-    h1 {
-      color: #3b404b;
-      font-family: SFProDisplay-Bold;
-      font-size: 50px;
-      line-height: 60px;
-      padding-top: 40px;
-      text-transform: capitalize;
-      padding-bottom: 16px;
-
-      span {
-        color: rgb(96, 191, 243);
-      }
-    }
-  }
-  .media-content {
-    background: white;
-    margin: unset;
-    // padding: 1.5rem;
-  }
-  // background: cadetblue;
-
-  .client-about-us {
-    display: flex;
-  }
-  // .description {
-  //   width: 100%;
-  //   margin: 10px;
-  //   background: white;
-  // }
-  .client-about-us,
-  .content {
-    background: white;
-    .image img {
-      width: 8rem;
-      height: 8rem;
-      border-radius: 5rem;
-      margin: auto;
-      background: #e8e8e8;
-    }
-  }
-
-  .profile {
-    height: 100%;
-    -moz-box-shadow: 0 0 3px #ccc;
-    -webkit-box-shadow: 0 0 3px #ccc;
-    box-shadow: 0 0 3px #ccc;
-    background: #fff;
-    // border-radius: 10px;
-  }
-
-  // .media {
-  //   padding: 20px;
-  //   margin: 0 26px;
-  //   position: relative;
-  //   background-color: #fff;
-  //   border-radius: 6px;
-  //   box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
-  //     0 0 0 1px rgba(10, 10, 10, 0.02);
-  //   min-height: 210px;
-  // }
-}
-
-@media only screen and (min-width: 360px) and (max-width: 640px) {
-  .about-us {
-    h5 {
-      padding: unset;
-      padding-bottom: 30px;
-    }
-    .media {
-      display: flex;
-      flex-direction: column;
-      margin: auto;
-
-      .media-left {
-        margin: auto;
-      }
-    }
-  }
-
-  .media-content {
-    .content {
-      text-align: center;
-      background: white;
-    }
-  }
-
-  .columns {
-    padding: 1rem 0 !important;
-    .column {
-      // padding: unset !important;
-    }
-  }
+  //   .columns {
+  //     padding: 1rem 0 !important;
+  //     .column {
+  //       // padding: unset !important;
+  //     }
+  //   }
 }
 </style>
